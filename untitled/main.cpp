6eead6888ba01e2283ad7f8747df92f6ac6da52d
alexey486 <morozov.aleksei@phystech.edu>
2022-02-10 19:08:41 +0200

    ipiho

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -3,9 +3,10 @@
 int main()
 {
     std::cout << "Hello Math!" << std::endl;
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World Master 2!" << std::endl;
     int a=0;
     std::cout<<a<<std::endl;
     int b=0;
     return 0;
+
 }
